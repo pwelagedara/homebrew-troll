ac2d4f17f2120957ad6c14b42fc0ad1a576454dd
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-14 16:09:15 -0500

    updated the repo

--- troll.rb
+++ troll.rb
@@ -1,8 +1,8 @@
 class Troll < Formula
   desc "Use this to troll idiots"
   homepage "https://github.com/pwelagedara/troll/"
-  url "https://github.com/pwelagedara/troll/archive/v2.0.0.tar.gz"
-  sha256 "33638e78a98695cf0bc68de51a5ac3abe4575f6c1510e4708648d8823d71e0c1"
+  url "https://github.com/pwelagedara/troll/archive/v2.0.1.tar.gz"
+  sha256 "e79e04b9442861e0a7af3ac53c8c3e03fe7285446b04991e68716234cba89d23"
 
   def install
     bin.install "bin/troll"
